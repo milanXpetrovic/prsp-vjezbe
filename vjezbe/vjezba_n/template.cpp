e4aa6656e33d586d8d6b89d68c898cab46a8a2ae
milanXpetrovic <milan.petrovic@uniri.hr>
2024-10-10 10:50:35 +0000

    added solution template

---
+++ vjezbe/vjezba_n/template.cpp
@@ -0,0 +1,18 @@
+// alias gpp='g++ -o ./solution'
+// alias gpp='g++ -o ./solution solution.cpp'
+
+#include <iostream>
+// #include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+
+int main(){
+    freopen("ulaz.txt", "r", stdin);
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    // Rjesenje zadatka
+
+}
