7c18dd789e8c26781ece6e61ecc3405c12600f43
RinoTomić <62605541+rtomic20@users.noreply.github.com>
2024-10-10 16:08:46 +0200

    CSES-weird_algorithm.cpp

--- vjezbe/vjezba_1/CSES-weird_algorithm.cpp
+++ vjezbe/vjezba_1/CSES-weird_algorithm.cpp
@@ -13,3 +13,23 @@ int main(){
     // Rjesenje zadatka
 
 }
+#include <iostream>
+using namespace std;
+int main() {
+    long long n;
+    cin>>n;
+    while(n>0){
+        if(n==1){
+            cout<<n<<"";
+            break;
+        }
+        if(n%2!=0){
+             cout<<n<<" ";
+             n=n*3+1;
+        }
+        else{
+            cout<<n<<" ";
+            n=n/2;
+        }
+    }
+}
