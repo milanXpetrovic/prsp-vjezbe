b7b6ceec2b44836a3ea9211592c3a51d85b602b8
milanXpetrovic <milan.petrovic@uniri.hr>
2024-10-10 13:53:04 +0000

    vjezba 2 added

---
+++ vjezbe/vjezba_2/zd_3_on.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+// #include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+
+int main(){
+    freopen("ulaz.txt", "r", stdin);
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    // Rjesenje zadatka
+
+}
