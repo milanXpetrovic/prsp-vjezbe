f081a7af83cc7ebb5e663f92154d566057042271
RinoTomić <62605541+rtomic20@users.noreply.github.com>
2024-10-10 19:15:39 +0200

    CF-long-words.cpp

--- vjezbe/vjezba_1/CF-long-words.cpp
+++ vjezbe/vjezba_1/CF-long-words.cpp
@@ -1,15 +1,33 @@
 #include <iostream>
+#include <vector>
+#include <string>
 // #include <bits/stdc++.h>
 
 using namespace std;
-
 typedef long long ll;
 
 int main(){
     freopen("ulaz.txt", "r", stdin);
     ios::sync_with_stdio(0);
     cin.tie(0);
-
-    // Rjesenje zadatka
+    int n;
+    cin >> n; 
+    
+    vector<string> words(n);  
+    
+    for (int i = 0; i < n; i++) {
+        cin >> words[i];  
+    }
+    
+    for (int i = 0; i < n; i++) {
+        if (words[i].length() > 10) {
+            cout << words[i][0] << words[i].length() - 2 << words[i][words[i].length() - 1] << endl;
+        } else {
+           
+            cout << words[i] << endl;
+        }
+    }
+    
+    return 0;
 
 }
