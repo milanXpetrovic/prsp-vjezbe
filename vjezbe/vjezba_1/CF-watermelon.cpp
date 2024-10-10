b7857a86b412e712f484b718e5a7872e14e94420
RinoTomić <62605541+rtomic20@users.noreply.github.com>
2024-10-10 16:03:59 +0200

    CF-watermelon.cpp

--- vjezbe/vjezba_1/CF-watermelon.cpp
+++ vjezbe/vjezba_1/CF-watermelon.cpp
@@ -13,3 +13,31 @@ int main(){
     // Rjesenje zadatka
 
 }
+#include <iostream>
+using namespace std;
+int main()
+{
+    long int w;
+    long int result;
+    cin>>w;
+    if(w==1){
+            cout<<"NO"<<endl;
+        }
+    for(long int i=1;i<w;i++){
+        if(w==2){
+            cout<<"NO"<<endl;
+            break;
+        }
+        if(i%2==0){
+            result=w%i;
+            if(result%2==0){
+                cout<<"YES"<<endl;
+                break;
+            }
+            else{
+                cout<<"NO"<<endl;
+                break;
+            }
+        }
+    }
+} 
